dfbf17246308911e7ecae6b0579751229cec5c25
DongHyeonWon <dhe7700@naver.com>
2023-11-07 02:47:52 +0900

    feat: Loading 추가

--- frontend/src/app/result/page.tsx
+++ frontend/src/app/result/page.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import Loading from "@/components/Loading";
+import React from "react";
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+  return <Loading />;
 }
---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionContainer from "../ui/SectionContainer";
+import LoadingTitle from "./title";
+import LoadingSubtitle from "./subtitle";
+import LoadingIcon from "../ui/icon/LoadingIcon";
+
+export default function Loading() {
+  return (
+    <SectionContainer className="flex flex-col h-[90%] items-center justify-center">
+      <LoadingTitle title="잠시만 기다려주세요" />
+      <LoadingSubtitle subtitle="미쓱이 사용자의 유형을 분석중이에요" />
+      <LoadingIcon className="w-8 h-8" />
+    </SectionContainer>
+  );
+}
