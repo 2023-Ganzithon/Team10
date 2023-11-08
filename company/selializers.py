ea78aa3f9d791fd1fa13e5d228e3fb6e7100d115
fjqmqjrm <fjqmqjrm@naver.com>
2023-11-08 13:43:28 +0900

    feat : 순위별 기업 정보 미리보기 + 기업별 댓글 커뮤니티

--- company/selializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Company
-
-#class CompanySerializer(serializers.ModelSerializer):
-    #class Meta:
-        #model = Company
-        #fields = '__all__'
