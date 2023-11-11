78e55dfd076d0f7e9e45c486767f22ea623ceb39
DongHyeonWon <dhe7700@naver.com>
2023-11-11 17:11:22 +0900

    feat: main page Loading 추가

--- frontend/src/components/MainSection/MainInfoSection/index.tsx
+++ frontend/src/components/MainSection/MainInfoSection/index.tsx
@@ -8,8 +8,8 @@ export default function MainInfoSection() {
       <SectionHeader title="ESG란?" linkHref="info" />
       <div className="mt-8 w-[20px] h-1 bg-brandColor mb-4" />
       <div className=" text-[15px]">
-        ESG는 환경, 사회, 재무구조 특면에서 중장기 기업가치에 직 · 간접적으로
-        영향을 미치는 비재무적 성과지표입니다.
+        ESG는 환경, 사회, 재무구조 특면에서 중장기 기업가치에 <br />직 ·
+        간접적으로 영향을 미치는 비재무적 성과지표입니다.
       </div>
     </SectionContainer>
   );
--- frontend/src/components/MainSection/MainSurveySection/MainUserScore/index.tsx
+++ frontend/src/components/MainSection/MainSurveySection/MainUserScore/index.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import SectionContainer from "@/components/ui/SectionContainer";
+import LoadingIcon from "@/components/ui/icon/LoadingIcon";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function MainUserScore() {
   const [userScore, setUserScore] = useState("?");
-  const [company, setCompany] = useState<string | null>("?");
+  const [company, setCompany] = useState<string | null>("");
   const [companyId, setCompanyId] = useState<string | null>("");
 
   useEffect(() => {
@@ -30,20 +31,25 @@ export default function MainUserScore() {
           href={userScore === "?" ? "/survey/0" : "/result"}
           className="w-[53px] h-[53px] rounded-full bg-buttonBackgroundColor flex justify-center items-center font-Pretendard-ExtraBold  text-brandColor"
         >
-          {userScore}
+          {company ? userScore : <LoadingIcon className="w-8" />}
         </Link>
       </div>
       <div className="flex  pl-4 py-8 items-center border-t-2  border-buttonBackgroundColor">
         <div className="text-[18px] font-semibold mr-4">추천 기업</div>
         <Link
           href={userScore === "?" ? "/survey/0" : `/company/${companyId}`}
-          className="w-[53px] h-[53px] rounded-full bg-buttonBackgroundColor flex justify-center items-center font-Pretendard-ExtraBold  text-brandColor"
+          className="w-[53px] h-[53px] rounded-full  flex justify-center items-center font-Pretendard-ExtraBold  text-brandColor"
         >
-          {/* {company ? (
-            <Image src={company} alt="companyLogo" width={50} height={10} />
+          {company ? (
+            <Image
+              src={`${company}`}
+              alt="companyLogo"
+              width={50}
+              height={10}
+            />
           ) : (
-            ""
-          )} */}
+            <LoadingIcon className="w-8" />
+          )}
           {/* <Image src={company || ""} alt="companyLogo" width={50} height={10} /> */}
         </Link>
       </div>
