b401320ef0e9d52d525213d26f8f51864638706f
fjqmqjrm <126189239+fjqmqjrm@users.noreply.github.com>
2023-12-06 12:55:52 +0900

    Update settings.py

--- mesg/settings.py
+++ mesg/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h4hnx3y9p6ls6@iy%vdj8j99a9bp*^1($+$hf_xziscm#3a3yb'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
